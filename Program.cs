5ac747e1dc8c8b399eb6475756658093d635bdec
netonblpb <skullbog@gmail.com>
2022-02-04 09:56:19 +0300

    Task10 complete

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Task10: Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+ Console.WriteLine("Вторая цифра: " + number[1]);
